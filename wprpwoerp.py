33fa424e3d8dda06c8744dc6ae5307de20a79106
Qweran <78544236+vadim0311@users.noreply.github.com>
2021-02-18 17:21:55 +0300

    Update wprpwoerp.py
    fin

--- wprpwoerp.py
+++ wprpwoerp.py
@@ -10,31 +10,72 @@ col = (0, 0, 0)
 i = 1
 x1 = 300
 y1 = 300
-w1 = 100
+w1 = 10
 x = 1
 
-# if x <= 100 and i == 1:
-#         x += 1
-#     if x >= 100:
-#         i = 0
-#         x -= 1
+
+def draw():
+    scr1.fill((0, 255, 255))
+    pg.draw.rect(scr1, (255, 255, 0), (100, 100, 400, 400))
+    pg.draw.rect(scr1, (255, 0, 0), (200, 200, 200, 200))
+    pg.draw.circle(scr1, col, (x1, y1), w1, w1,)
+    pg.display.update() 
+
+    
 
 while True:
     screen()
-    scr1.fill((255, 255, 255))
-    pg.draw.circle(scr1, col, (x1, y1), w1, w1,)
+    draw()
     keys = pg.key.get_pressed()
-    if keys[pg.K_DOWN]:
-        y1 += 3
-    if keys[pg.K_UP]:
-        y1 -= 3
-    if keys[pg.K_RIGHT]:
-        x1 += 3
-    if keys[pg.K_LEFT]:
-        x1 -= 3
-    if keys[pg.K_w]:
-        w1 += 3
-    if keys[pg.K_s]:
-        w1 -= 3
-    pg.display.update()
+    if y1 > 500 or y1 < 100 or x1 < 100 or x1 > 500:
+        col = (255, 0, 255)
+        w1 = 10
+        if keys[pg.K_DOWN]:
+            y1 += 5
+        if keys[pg.K_UP]:
+            y1 -= 5 
+        if keys[pg.K_RIGHT]:
+            x1 += 5
+        if keys[pg.K_LEFT]:
+            x1 -= 5
+    elif y1 > 400 or y1 < 200 or x1 < 200 or x1 > 400:
+        w1 = 20
+        col = (255, 0, 0)
+        if keys[pg.K_DOWN]:
+            y1 += 3
+        if keys[pg.K_UP]:
+            y1 -= 3 
+        if keys[pg.K_RIGHT]:
+            x1 += 3
+        if keys[pg.K_LEFT]:
+            x1 -= 3
+    else:
+        w1 = 30
+        col = (0, 0, 0)
+        if keys[pg.K_DOWN]:
+            y1 += 1
+        if keys[pg.K_UP]:
+            y1 -= 1 
+        if keys[pg.K_RIGHT]:
+            x1 += 1
+        if keys[pg.K_LEFT]:
+            x1 -= 1
+    if x1 >= 600 + w1:
+        x1 = 0
+    if x1 <= 0:
+        x1 = 600
+    if y1 >= 600 + w1:
+        y1 == 0
+    if y1 <= 0:
+        y1 = 600 
+    if not(keys[pg.K_UP] or keys[pg.K_LEFT] or keys[pg.K_RIGHT] or keys[pg.K_DOWN]):
+        if y1 > 300:
+            y1 -= 3
+        if x1 > 300:
+            x1 -= 3
+        if y1 < 300:
+            y1 += 3
+        if x1 < 300:
+            x1 += 3
+    
     
